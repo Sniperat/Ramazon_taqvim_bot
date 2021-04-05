aff6b6cf7bcf4f4a85e291ddcee530486cb71fbf
ali <muhammadalixonakbarov2000@gmail.com>
2021-04-05 15:30:49 +0500

    second

--- bot_ramazon/management/commands/bot.py
+++ bot_ramazon/management/commands/bot.py
@@ -4,14 +4,19 @@ from ._base import BotBase
 import json, requests
 import datetime
 from ...functions import user_func
+from ...models import Region
 
 api = 'https://api.aladhan.com/timingsByAddress?address=Tashkent,%20UK&method=99&methodSettings=18.5,null,17.5'
 response = requests.get(api)
-# print(response.json())
+time = response.json()
 
-time = json.dumps(response.json())
-print(time)
-print(datetime.datetime.now())
+
+# print(time)
+# print(datetime.datetime.now())
+taqvim = time['data']['timings']
+kun = time['data']['date']['readable']
+
+
 
 
 class Command(BotBase):
@@ -23,16 +28,61 @@ class Command(BotBase):
 
         keyboard = [
             [
-                InlineKeyboardButton('Курси', callback_data='course'),
-                InlineKeyboardButton('Интенсивы', callback_data='2')
+                InlineKeyboardButton('Manzilni tanlash', callback_data='region')
             ],
 
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
 
-        update.message.reply_text("Salam", reply_markup=reply_markup)
+        update.message.reply_text("Assalamu aleykum va rahmatullohu va barokatuhu\nAniq vaqt uchun o'z manzilingizni belgilang", reply_markup=reply_markup)
 
+    def region(self, update: Update, context: CallbackContext) -> None:
+        user = user_func(update)
+        region = Region.objects.all()
+        query = update.callback_query
+        query.answer()
+        keyboard = []
 
+        for i in region:
+            keyboard.append([
+                InlineKeyboardButton(i.name, callback_data=i.id)
+            ],)
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        query.edit_message_text(text='Manzilni tanlash', reply_markup=reply_markup)
+    def button(self, update: Update, context: CallbackContext) -> None:
+        user = user_func(update)
+        query = update.callback_query
+        query.answer()
+        region = Region.objects.get(id=query.data)
+        keyboard = []
+        if str(query.data) == str(region.id):
+            user.region_ID = region
+            user.save()
+            keyboard = [
+                [
+                    InlineKeyboardButton("Economy", callback_data='comnE')
+
+                ],
+                [
+                    InlineKeyboardButton("Standart", callback_data='comnS')
+
+                ],
+                [
+                    InlineKeyboardButton("Deluxe", callback_data='comnD')
+
+                ],
+                [
+                    InlineKeyboardButton("Luxe", callback_data='comnL')
+
+                ],
+                [
+                    InlineKeyboardButton("Назад", callback_data='asd')
+
+                ]
+            ]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            query.edit_message_text(text='asda', reply_markup=reply_markup)
     def handle(self, *args, **kwargs):
         dispatcher = self.updater.dispatcher
 
@@ -42,9 +92,9 @@ class Command(BotBase):
         # dispatcher.add_handler(CallbackQueryHandler(self.about, pattern="^(about)$"))
         # dispatcher.add_handler(CallbackQueryHandler(self.course, pattern="^(course)$"))
         # dispatcher.add_handler(CallbackQueryHandler(self.course1, pattern="^(course1)$"))
-        # dispatcher.add_handler(CallbackQueryHandler(self.money, pattern="^(money)$"))
+        dispatcher.add_handler(CallbackQueryHandler(self.region, pattern="^(region)$"))
         dispatcher.add_handler(CommandHandler('start', self.start))
-        # dispatcher.add_handler(CallbackQueryHandler(self.button))
+        dispatcher.add_handler(CallbackQueryHandler(self.button))
 
         # dispatcher.add_handler(MessageHandler(Filters.all & ~Filters.command, self.message_handler))
 
--- bot_ramazon/models.py
+++ bot_ramazon/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 
+class Region(models.Model):
+    name = models.CharField(max_length=50)
+    time = models.CharField(max_length=5)
+
+
 class User(models.Model):
     telegram_user_id = models.BigAutoField(primary_key=True)
     user_name = models.CharField(max_length=50, default=None, null=True)
-    region = models.CharField(max_length=5, default=None, null=True)
-
-
+    region_ID = models.ForeignKey(Region, on_delete=models.CASCADE, null=True, default=None)
